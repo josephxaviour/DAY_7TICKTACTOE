6ed282c02cb7ddc9b55089192fa01db500d1f6ce
josephxaviour <josephxaviour007@gmail.com>
2022-12-06 12:43:51 +0530

    [joseph] UC2_TicTacToe

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -3,3 +3,18 @@ package com.bridgelabz;
 public class TicTacToe {
 static char[] board = new char[0];
 }
+public class TicTakToeProblem {
+    static char[] board = new char[0];
+
+    char input() {
+        int randomChar = (int) Math.floor(Math.random() * 10 % 2);
+        if (randomChar == 1) {
+            return 'x';
+        } else {
+            return '0';
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
