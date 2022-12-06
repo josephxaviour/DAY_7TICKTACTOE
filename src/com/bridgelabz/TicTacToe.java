20ba223a9be2dc263c783431ea14281e3d470512
josephxaviour <josephxaviour007@gmail.com>
2022-12-06 13:40:17 +0530

    [joseph]UC4_TicTacToe

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,5 +1,9 @@
 package com.bridgelabz;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class TicTacToe {
 static char[] board = new char[0];
 }
@@ -18,3 +22,54 @@ public class TicTakToeProblem {
         
     }
 }
+public class TicTacToeProblem {
+
+    static String[] board = new String[9];
+    static String turn;
+     static void printBoard() {
+         for (int i=0; i< board.length; i++) {
+             System.out.println(board[i]);
+         }
+     }
+static String checkWinner() {
+
+     for (int a=0; a<9;a++) {
+         if(Arrays.asList(board).contains(
+                 String.valueOf(a + 1))) {
+             break;
+         } else if (a == 8) {
+             return "draw";
+         }
+
+         return null;
+         }
+
+     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String winner = null;
+        turn = 'x';
+        while (winner == null) {
+            int numInput = in.nextInt();
+            for(int a=0; a<9;a++) {
+                board[a] = String.valueOf(a + 1);
+            }
+            if (board[numInput - 1].equals(
+                    String.valueOf(numInput))) {
+                board[numInput - 1] = turn;
+
+                if (turn.equals("x")) {
+                    turn = "0";
+                } else {
+                    turn = "x";
+
+                }
+                printBoard();
+                winner = checkWinner();
+            }else {
+                System.out.println("slot allready taken, re-enter slot no");
+            }
+            }
+            }
+        }
+    }
+
